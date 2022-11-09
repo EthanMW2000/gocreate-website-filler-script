00cdb23b5288dae6b73923b9cde4457e1fde1d5e
Ethan Wells <ethanmw2000@gmail.com>
2022-11-09 09:02:43 -0600

    renamed file and fucntion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pkg.file_opener as file_opener
 import pkg.excel_parser as excel_parser
-import pkg.website_filler as website_filler
+import pkg.setup_website as setup_website
 
 
 def main():
@@ -8,7 +8,7 @@ def main():
   if not filepath:
     return print('No file selected. Closing program.')
   parsedfile = excel_parser.parse_excel(filepath)
-  website_filler.fill_website(parsedfile)
+  setup_website.setup(parsedfile)
   
 
 
