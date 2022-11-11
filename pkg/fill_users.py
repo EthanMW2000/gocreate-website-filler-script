bcf2ca36de706bb0829df6a638b5bfc55ebe0f2d
Ethan Wells <ethanmw2000@gmail.com>
2022-11-11 15:07:02 -0600

    Completed finding users

--- pkg/fill_users.py
+++ pkg/fill_users.py
@@ -1,5 +1,26 @@
+import pandas as pd
+from selenium.webdriver import Chrome
 from selenium.webdriver.common.by import By
+from tkinter.messagebox import Message
 
-def fill_users(file, browser):
+
+def fill_users(file: pd.DataFrame, browser: Chrome):
+  user_not_found = []
   searchbar = browser.find_element(By.CSS_SELECTOR, 'search')
-  
+  file.reset_index()
+  for index, row in file.iterrows():
+    searchbar.send_keys(row.loc['Email'])
+    firstName = row.loc['Full Name'].split(' ')[0]
+    try:
+      user = browser.find_element(By.XAPTH(f"//<>[text()='{firstName}']"))
+    except:
+      user_not_found.append(row.loc['Email'])
+      continue
+    else:
+      user.click()
+      fill_info(file, browser)
+      
+    searchbar.clear()
+    
+def fill_info(file: pd.DataFrame, browser: Chrome):
+  print('') 
