8547bba19226ee1728ee755231b0dc26ffb77294
Ethan Wells <ethanmw2000@gmail.com>
2022-11-12 11:16:21 -0600

    made more fill users additions

--- pkg/fill_users.py
+++ pkg/fill_users.py
@@ -1,26 +1,39 @@
-import pandas as pd
+from pandas import DataFrame
 from selenium.webdriver import Chrome
 from selenium.webdriver.common.by import By
 from tkinter.messagebox import Message
+from pkg.set_dob import set_dob
+from set_contact import set_contact
+from set_emergency_contact import set_emergency_contact
 
 
-def fill_users(file: pd.DataFrame, browser: Chrome):
+def fill_users(file: DataFrame, browser: Chrome):
   user_not_found = []
-  searchbar = browser.find_element(By.CSS_SELECTOR, 'search')
   file.reset_index()
-  for index, row in file.iterrows():
-    searchbar.send_keys(row.loc['Email'])
-    firstName = row.loc['Full Name'].split(' ')[0]
+  
+  searchbar = browser.find_element(By.CSS_SELECTOR, 'search')
+  
+  for row in file.iterrows():
+    searchbar.send_keys(row[1]['Email'][:])
+    firstName = row[1]['Full Name'][:].split(' ')[0]
+    
     try:
-      user = browser.find_element(By.XAPTH(f"//<>[text()='{firstName}']"))
+      user = browser.find_element(By.XAPTH(f"//[@text()='{firstName}']"))
     except:
-      user_not_found.append(row.loc['Email'])
+      user_not_found.append(row[1]['Email'][:])
       continue
     else:
       user.click()
-      fill_info(file, browser)
-      
+      fill_info(row, browser)
+
     searchbar.clear()
     
-def fill_info(file: pd.DataFrame, browser: Chrome):
-  print('') 
+def fill_info(row: tuple, browser: Chrome):
+  set_dob(row, browser)
+  browser.find_element(By.XPATH(
+    "//input[@type='checkbox' and @data-flag='membership_agreement']"
+    )).click()
+  set_contact(row, browser)
+  set_emergency_contact(row, browser)
+  
+  
