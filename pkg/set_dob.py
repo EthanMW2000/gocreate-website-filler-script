b79a44a87725c6210c68b6bbc7490b7790ceef00
Ethan Wells <ethanmw2000@gmail.com>
2022-11-12 09:10:32 -0600

    created func to set birthdate in user profile

---
+++ pkg/set_dob.py
@@ -0,0 +1,22 @@
+from selenium.webdriver import Chrome
+from selenium.webdriver.common.by import By
+
+
+def set_dob(row: tuple, browser: Chrome):
+  browser.find_element(By.XPATH("//a[@data-original-title='Edit']")).click()
+  
+  dob = browser.find_element(By.XPATH("//input[@name='dob']"))
+  
+  user_dob = row[1]['Birthdate'][:]
+  if not '/' in user_dob:
+    if '-' in user_dob:
+      user_dob = user_dob.replace('-', '/')
+    else:
+      user_dob = user_dob[:2] + '/' + user_dob[2:4] + '/' + user_dob[4:]
+      
+  dob.send_keys(user_dob)
+  browser.find_element(By.XPATH(
+    "(//button[@type='submit' and contains(., 'Save Changes')])[1]"
+    )).click()
+  
+  return
