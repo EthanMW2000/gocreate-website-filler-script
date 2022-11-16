6b88c2886343daf727af679627bf9fda3931182e
Ethan Wells <ethanmw2000@gmail.com>
2022-11-16 14:05:21 -0600

    Started payment info section

---
+++ pkg/set_payments.py
@@ -0,0 +1,31 @@
+import datetime
+
+from selenium.webdriver import Chrome
+from selenium.webdriver.common.by import By
+
+
+def set_payments(row: tuple, browser: Chrome):
+  browser.find_element(By.XPATH, "//a[@aria-controls='payments']").click()
+  browser.find_element(By.XPATH, "//button[@data-target='#new_subscription_modal']").click()
+  
+  currentdate = datetime.datetime.now()
+  startdate = browser.find_element(By.NAME, 'start')
+  startdate.send_keys(currentdate.strftime("%m/%d/%Y"))
+  
+  enddate_input = generate_enddate(currentdate)
+  enddate = browser.find_element(By.NAME, 'end')
+  enddate.send_keys(enddate_input)
+  
+  return
+
+def generate_enddate(currentdate: datetime.datetime) -> str:
+  #enddate = currentdate + datetime.timedelta(days=30)
+  
+  if (currentdate.month > 5 and currentdate.month < 12):
+    enddate = datetime.datetime(currentdate.year, 12, 31)
+  elif (currentdate.month <= 5):
+    enddate = datetime.datetime(currentdate.year, 5, 31)
+  elif (currentdate.month == 12):
+    enddate = datetime.datetime(currentdate.year + 1, 5, 31)
+  
+  return enddate.strftime("%m/%d/%Y")
