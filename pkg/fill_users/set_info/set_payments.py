0919926df8dfcc148ccbd7033e10405bfe77f371
Ethan Wells <ethanmw2000@gmail.com>
2022-11-17 12:32:12 -0600

    finsihed set payments function

--- pkg/fill_users/set_info/set_payments.py
+++ pkg/fill_users/set_info/set_payments.py
@@ -2,9 +2,10 @@ import datetime
 
 from selenium.webdriver import Chrome
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
 
 
-def set_payments(row: tuple, browser: Chrome):
+def set_payments(browser: Chrome):
   browser.find_element(By.XPATH, "//a[@aria-controls='payments']").click()
   browser.find_element(By.XPATH, "//button[@data-target='#new_subscription_modal']").click()
   
@@ -15,6 +16,26 @@ def set_payments(row: tuple, browser: Chrome):
   enddate_input = generate_enddate(currentdate)
   enddate = browser.find_element(By.NAME, 'end')
   enddate.send_keys(enddate_input)
+  
+  paid_installments = browser.find_element(By.NAME, 'paid_installments')
+  paid_installments.send_keys('1')
+  total_installments = browser.find_element(By.NAME, 'total_installments')
+  total_installments.send_keys('1')
+  
+  payment_mode = Select(browser.find_element(By.NAME, 'payment_mode'))
+  payment_mode.select_by_visible_text('WSU Comp/Prize')
+  
+  amount = browser.find_element(By.NAME, 'amount')
+  amount.send_keys('0')
+  total_amount = browser.find_element(By.NAME, 'total_amount')
+  amount.send_keys('0')
+  
+  auxiliary = browser.find_element(By.NAME, 'auxiliary')
+  auxiliary.send_keys(f'WSU Student Free Membership {currentdate.strftime("%m/%d/%Y")} - Admin Script')
+  
+  browser.find_element(By.XPATH,
+    "(//button[@type='submit' and contains(., 'Save Changes')])[4]"
+  ).click()
   
   return
 
