a8197f22dfe49e37e9b588c48ea7828a89e40908
Ethan Wells <ethanmw2000@gmail.com>
2022-11-17 14:04:50 -0600

    setup messagebox

--- src/main.py
+++ src/main.py
@@ -2,12 +2,13 @@ from pkg.file_opener.file_opener import open_file
 from pkg.excel_parser.excel_parser import parse_excel
 from pkg.setup_website.setup_website import setup
 from pkg.fill_users.fill_users import fill_users
+from tkinter import messagebox
 
 
 def main():
   filepath = open_file()
   if not filepath:
-    return print('No file selected. Closing program.')
+    return messagebox.Message('No File Selected', 'No file was selected.')
   parsedfile = parse_excel(filepath)
   browser = setup()
   fill_users(parsedfile, browser)
