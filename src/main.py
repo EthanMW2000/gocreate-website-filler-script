e87f025dc19e788d8111091cb2e1518a166f7229
Ethan Wells <ethanmw2000@gmail.com>
2022-11-18 10:04:29 -0600

    moved files

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
-from pkg.file_opener.file_opener import open_file
-from pkg.excel_parser.excel_parser import parse_excel
-from pkg.setup_website.setup_website import setup
-from pkg.fill_users.fill_users import fill_users
+from pkg.file_opener import open_file
+from pkg.excel_parser import parse_excel
+from pkg.setup_website import setup
+from pkg.fill_users import fill_users
 from tkinter import messagebox
 
 
